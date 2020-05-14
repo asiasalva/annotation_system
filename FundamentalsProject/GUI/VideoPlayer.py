7536014d217ea87673615f38dc6640b2f6d9ac3c
Brugix <Brugix@users.noreply.github.com>
2020-05-14 20:14:02 +0200

    2020-05-14
    Drawing works.
    But QVideoWidget overlay is not working.

--- FundamentalsProject/GUI/VideoPlayer.py
+++ FundamentalsProject/GUI/VideoPlayer.py
@@ -1,10 +1,16 @@
 from PyQt5 import QtCore, QtWidgets
-from PyQt5.QtWidgets import (QVBoxLayout, QWidget , QSlider)
+from PyQt5.QtWidgets import (QVBoxLayout, QWidget , QSlider, QLabel)
 
 from PyQt5.QtMultimedia import QMediaContent, QMediaPlayer
 from PyQt5.QtMultimediaWidgets import QVideoWidget
 from PyQt5.QtCore import Qt, QUrl
+from PyQt5 import QtGui
+from PyQt5.QtGui import QPixmap
 
+
+import GUI
+from GUI import WindowPaint
+from PyQt5.QtWidgets import QStackedLayout, QGridLayout
 
 
 class VideoPlayer(QWidget):
@@ -26,23 +32,52 @@ class VideoPlayer(QWidget):
 		self.positionSlider = QSlider(Qt.Horizontal)
 		self.positionSlider.setRange(0, 0)
 		self.positionSlider.sliderMoved.connect(self.goToPosition)
+		
 
+
+		self.x = QLabel("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
+		self.x.setStyleSheet("background-color: rgba(45, 145, 75, 0)")
+		self.x.setAlignment(Qt.AlignHCenter | Qt.AlignCenter)
+
+		self.windowPaint = WindowPaint.WindowPaint()
+		self.windowPaint.setupUi(MainWindow)
+
+		#self.layout = QStackedLayout()
+		#self.layout.addWidget(self.videoWidget)
+		#self.layout.addWidget(self.windowPaint)
+		##self.layout.addWidget(self.x)
+		#self.layout.setStackingMode(QStackedLayout.StackAll)#StackOne)
+		##self.layout.setCurrentIndex(0)
+
+		#self.windowPaint.image.fill(Qt.red)
+		#self.background = QLabel(self)
+		#self.background.setPixmap(QPixmap("C:\\Users\\Brugix\\source\\repos\\FundamentalsProject\\FundamentalsProject\\GUI\\12.jpg"))
+
+		#self.layout = QGridLayout()
+		
+		
+		
+		#self.layout.addWidget(self.background, 0,0)
+		#self.layout.addWidget(self.windowPaint, 0,0)
+		#self.layout.addWidget(self.videoWidget, 0,0)
+		#self.layout.addWidget(self.x, 0,0)
 
 
 		### Widget container
 		container = QVBoxLayout(self)
 		container.addWidget(self.videoWidget)
+		#container.addLayout(self.layout)
 		container.addWidget(self.positionSlider)
 
 
+		self.time = 1000
 
+		fileName = "C:\\Users\\Brugix\\source\\repos\\FundamentalsProject\\FundamentalsProject\\GUI\\video.mp4"
+		#fileName = "D:\\Videos\\Star Wars Episode III - Revenge of the Sith (2005).mkv"
+		self.mediaPlayer.setMedia(QMediaContent(QUrl.fromLocalFile(fileName)))
+		self.mediaPlayer.play()
 
-		#fileName = "C:\\Users\\Brugix\\source\\repos\\FundamentalsProject\\FundamentalsProject\\GUI\\video.mp4"
-		#self.mediaPlayer.setMedia(QMediaContent(QUrl.fromLocalFile(fileName)))
-		#self.mediaPlayer.play()	
-
-
-
+		
 
 
 	### Functions of VideoPlayer
@@ -54,10 +89,15 @@ class VideoPlayer(QWidget):
 	# Change slider position as video go forward/backward
 	def positionChanged(self, position):
 		self.positionSlider.setValue(position)
+		if position > self.time/2:
+			self.layout.setCurrentIndex(1)
 
 	# Change slider moving speed
 	def durationChanged(self, duration):
 		self.positionSlider.setRange(0, duration)
+		self.time = duration
+
+		print("4 -> " + str(self.mediaPlayer.metaData("Resolution")))
 
 
 
